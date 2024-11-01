a77285c314546ce6cdcea388b67b19fe957f38a0
TotallyScience <42273339+TotallyScience@users.noreply.github.com>
2024-11-01 17:25:16 -0400

    Add files via upload

---
+++ assets/includes/footer.php
@@ -0,0 +1,46 @@
+<footer>
+    <div class="footSections">
+        <div class="footSection">
+            <h1>Contact Us</h1>
+            <p><a href="mailto: help@totallyscience" target=”_blank”>help@totallyscience.co</a></p>
+            <p><a href="https://discord.com/invite/6kTskTSTFM" target=”_blank”>Join our Discord</a></p>
+            <p><a href="https://www.tiktok.com/@totallyscience.co" target=”_blank”>Follow our Tik Tok</a></p>
+        </div>
+
+        <div class="footSection">
+            <h1>Our Team</h1>
+
+            <div class="teamFoot">
+                <div>Zach Yadegari</div>
+
+                <div>||</div>
+
+                <div>Founder & CEO</div>
+
+                <div>Ilan</div>
+
+                <div>||</div>
+
+                <div>CTO</div>
+            </div>
+        </div>
+
+        <div class="footSection">
+            <h1>Legal</h1>
+
+            <p><a href="termsofuse.php">Terms of Use</a></p>
+            <p><a href="privacypolicy.php">Privacy Policy</a></p>
+            <p><a href="partners.php">Partners</a></p>
+        </div>
+    </div>
+
+    <div class="bottomFoot">© 2023 TotallyScience. A Zach Yadegari Production ™</div>
+</footer>
+<script src="assets/scripts/main.js?v77"></script>
+<!-- Info link code -->
+<script type="text/javascript">
+var infolinks_pid = 3412985;
+var infolinks_wsid = 0;
+</script>
+<script type="text/javascript" src="//resources.infolinks.com/js/infolinks_main.js"></script>
+<!-- END of Info link code -->
---
+++ assets/includes/head.php
@@ -0,0 +1,79 @@
+<!-- Google Tag Manager -->
+<!-- <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KGRTD4B');</script> -->
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-BW3SZSE47T"></script>
+<script>
+window.dataLayer = window.dataLayer || [];
+
+function gtag() {
+    dataLayer.push(arguments);
+}
+gtag('js', new Date());
+
+gtag('config', 'G-BW3SZSE47T');
+</script>
+<!-- End Google Tag Manager -->
+
+<title>Totally Science - Free Online Unblocked Games</title>
+
+<meta property="og:title" content="Totally Science - Free Online Unblocked Games">
+<meta property="og:site_name" content="Totally Science">
+<meta property="og:url" content="https://totallyscience.co">
+<meta property="og:description"
+    content="Play free online games at Totally Science - your one-stop destination for free online games! io games, puzzle games, Flash games, shooting games and more. And why &quot;Totally Science&quot;? It's our secret for bypassing school blockers. Sit back, relax, and enjoy learning...science?">
+<meta property="og:type" content="website">
+<meta property="og:image" content="https://totallyscience.co/assets/images/totally-science-logo-transparent-scaled.png">
+<meta charset="UTF-8">
+<meta name="google-site-verification" content="-gcWvd6d4OTZeFW4HWaIIZTNKnnOfAx15pEw_XDAH2o" />
+<meta name="url" content="https://totallyscience.co">
+<meta name="owner" content="Zach Yadegari">
+<meta name="designer" content="Zach Yadegari">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="robots" content="index, follow">
+<meta name="twitter:title" content="Totally Science">
+<meta name="DC.format" content="text/html" scheme="DCTERMS.DCMIType">
+<meta name="DC.publisher" content="Totally Science" scheme="DCTERMS.DCMIType">
+<meta name="DC.description"
+    content="Play free online games at Totally Science - your one-stop destination for free online games! io games, puzzle games, Flash games, shooting games and more. And why &quot;Totally Science&quot;? It's our secret for bypassing school blockers. Sit back, relax, and enjoy learning...science?"
+    scheme="DCTERMS.DCMIType">
+<meta name="DC.subject" content="totally science, games, gaming, online games, unblocked games"
+    scheme="DCTERMS.DCMIType">
+<meta name="DC.creator" content="Zach Yadegari">
+<meta name="subject" content="totally science, games, gaming, online games, unblocked games">
+<meta name="keywords"
+    content="totally science, games, gaming, online games, unblocked games, discord unblocked, minecraft online, roblox unblocked">
+<meta name="twitter:description"
+    content="Play free online games at Totally Science - your one-stop destination for free online games! io games, puzzle games, Flash games, shooting games and more. And why &quot;Totally Science&quot;? It's our secret for bypassing school blockers. Sit back, relax, and enjoy learning...science?">
+<meta name="twitter:url" content="https://totallyscience.co">
+<meta name="twitter:image"
+    content="https://totallyscience.co/assets/images/totally-science-logo-transparent-scaled.png">
+<meta name='description'
+    content="Play free online games at Totally Science - your one-stop destination for free online games! io games, puzzle games, Flash games, shooting games and more. And why &quot;Totally Science&quot;? It's our secret for bypassing school blockers. Sit back, relax, and enjoy learning...science?">
+<meta name="theme-color" content="#f75dfc">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<link rel="preload" href="assets/Rubik.woff2" as="font" type="font/woff2" crossorigin="anonymous">
+
+<!-- WG Universal adCode -->
+<script type="text/javascript" async>
+! function(e, t) {
+    a = e.createElement("script"), m = e.getElementsByTagName("script")[0], a.async = 1, a.src = t, m.parentNode
+        .insertBefore(a, m)
+}(document, "https://universal.wgplayer.com/tag/?lh=" + window.location.hostname + "&wp=" + window.location
+    .pathname + "&ws=" + window.location.search);
+</script>
+
+<script src="../assets/scripts/sweetalert.min.js"></script>
+
+<!-- Lazy loading -->
+<script src="https://cdn.polyfill.io/v2/polyfill.min.js?features=IntersectionObserver"></script>
+
+
+
+
+<link rel="stylesheet" href="assets/styles/main.css?v48">
+<link rel="icon" href="assets/images/totally-science-logo-transparent-scaled.png">
